2d7b051c1397297e0544dea6857101802f17426b
celianLaumond <celian.laumond@my-digital-school.org>
2025-02-20 11:58:53 +0100

    sql data

---
+++ dump_data/user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (id, email, roles, password, program_id) VALUES
+                                                               (1, 'user1@example.com', '["ROLE_USER"]', 'hashedpassword1', NULL),
+                                                               (2, 'user2@example.com', '["ROLE_USER"]', 'hashedpassword2', NULL),
+                                                               (3, 'user3@example.com', '["ROLE_USER"]', 'hashedpassword3', NULL),
+                                                               (4, 'user4@example.com', '["ROLE_USER"]', 'hashedpassword4', NULL),
+                                                               (5, 'user5@example.com', '["ROLE_USER"]', 'hashedpassword5', NULL);
